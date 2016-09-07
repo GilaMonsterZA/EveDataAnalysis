1e4befa7c3b68596dc728f90aa89b08b3eacca6a
Gail <gail@gilamonster.za.net>
2016-09-07 22:59:31 +0200

    Error handling on market price fetches

--- FetchMarketPrices.R
+++ FetchMarketPrices.R
@@ -16,18 +16,26 @@ loadMarketPrices <- function(items, useSystem) {
 RegionPricesAsOf <- function(Region, TypeID, Dates) { # Dates as a list of strings in the format '2016-04-02T00:00:00', Example region = 10000002 (The Forge), Example TypeID = 34 (Trit)
   require(jsonlite)
   priceHistory_addr <- paste("https://crest-tq.eveonline.com/market/",Region,"/history/?type=https://crest-tq.eveonline.com/inventory/types/",TypeID,"/", sep= "") 
-  suppressWarnings(market.json <- fromJSON(readLines(priceHistory_addr))) # throws a warning about incomplete final line
-  MarketValues <- market.json$items
-  MarketPrices <- numeric(length(Dates))
-  for (i in seq_along(Dates)) {
-    MarketPrices[i] <- as.numeric(MarketValues[MarketValues$date==Dates[[i]],5])
+  suppressWarnings(market.json <- try(fromJSON(readLines(priceHistory_addr)), silent=TRUE)) # throws a warning about incomplete final line
+  if (class(market.json) == "try-error") {
+    MarketPrices <- numeric(length(Dates))
+  }
+  else {
+    MarketValues <- market.json$items
+    MarketPrices <- numeric(length(Dates))
+    for (i in seq_along(Dates)) {
+      price <- as.numeric(MarketValues[MarketValues$date==Dates[[i]],5])
+      if (length(price) == 0) {
+        price <- 0
+      }
+      MarketPrices[i] <- price
+    }
   }
   MarketPrices
 }
 
-getItemPrices <- function (Region, TypeIDs, Dates) { #to finish
-  require(jsonlite)
-  
+getItemPrices <- function (Region, TypeIDs, Dates) { 
+
   ItemPrices <- data.frame(matrix(0.0, ncol = length(Dates), nrow = length(TypeIDs)))
   colnames(ItemPrices) <- Dates
   
